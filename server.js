90f70ea9b99a5f6bb885ad629067a3e3cb352263
Robert <rweitman@smu.edu>
2017-08-04 13:30:28 -0500

    Creation

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(PORT, function() {
-	console.log("listening on " + PORT);
+app.listen(port, function() {
+	console.log("listening on " + port);
 });
