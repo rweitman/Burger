ee825545de44774364a6c27589751f51cfe7ca5e
Robert <rweitman@smu.edu>
2017-08-08 18:48:25 -0500

    Trial

--- db/schema2.sql
+++ db/schema2.sql
@@ -1,9 +1,8 @@
 USE heroku_190559482844405;
 
-DROP TABLE IF EXISTS burgers;
-
 SET @@SESSION.auto_increment_increment = 1;
 
+DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
